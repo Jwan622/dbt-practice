df410bab3339a2d037e685d81723eff000382eff
Jeffrey <jwan622@gmail.com>
2024-02-04 15:16:20 -0500

    add 28d rolling test and red/green the test

--- tests/ticket_sales/test_ticket_sales_counts_are_valid.sql
+++ tests/ticket_sales/test_ticket_sales_counts_are_valid.sql
@@ -8,15 +8,23 @@ WITH cte AS (
     SELECT
     tsc.date,
     tsc.rolling_7_days_total AS actual_7d_total,
+    tsc.rolling_28_days_total AS actual_28d_total,
     SUM(tsc.daily_total_tickets) OVER (
-        ORDER BY tsc.date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS expected_7d_total -- 6 not 7! god dammit.
+        ORDER BY tsc.date ASC ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS expected_7d_total, -- 6 not 7! god dammit.
+    SUM(tsc.daily_total_tickets) OVER (
+        ORDER BY tsc.date ASC ROWS BETWEEN 27 PRECEDING AND CURRENT ROW
+    ) AS expected_28d_total
     FROM dbt.ticket_sales_counts tsc
 )
 SELECT
     '7-day rolling total matches daily_total_tickets sum for last 7 days' AS test,
     date,
     actual_7d_total,
-    expected_7d_total
+    expected_7d_total,
+    actual_28d_total
+    expected_28d_total
 FROM cte
 WHERE actual_7d_total <> expected_7d_total
+OR actual_28d_total <> expected_28d_total
 
